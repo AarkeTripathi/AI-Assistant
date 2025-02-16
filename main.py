6146d9c73e83f1c8a8b28418e1cdf38f6075afc2
AarkeTripathi <aarke2002tripathi@gmail.com>
2025-02-16 22:56:35 +0530

    Corrected IP and Port

--- main.py
+++ main.py
@@ -160,5 +160,6 @@ async def image_processing(session_id: str, text: Optional[str] = Form(None), fi
 
 
 if __name__=="__main__":
-   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+   port = int(os.getenv("PORT", 8000))
+   uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
    db.conn.close()
