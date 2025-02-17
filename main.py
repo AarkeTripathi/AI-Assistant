5177d01d22696fe885fe01cc4935d86ec6a8c1a8
AarkeTripathi <aarke2002tripathi@gmail.com>
2025-02-17 13:23:32 +0530

    Remove reload

--- main.py
+++ main.py
@@ -161,5 +161,5 @@ async def image_processing(session_id: str, text: Optional[str] = Form(None), fi
 
 if __name__=="__main__":
    port = int(os.getenv("PORT", 8000))
-   uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+   uvicorn.run("main:app", host="0.0.0.0", port=port)
    db.conn.close()
